5f19d265bca30218dad3f3af99dfc11e075e91f8
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 22:06:52 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author
   end
 
   def new
@@ -9,13 +9,35 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
+    if @author.valid?
+       @author.save
+       redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    set_author
+  end
+
+  def update
+    set_author
+    if @author.valid?
+      @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 end
